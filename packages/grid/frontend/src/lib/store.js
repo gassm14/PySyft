9aec75fcd3d8641e4c6cebc7a529eb83eef942be
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-02-15 10:12:48 +0000

    style: prettier pass

--- packages/grid/frontend/src/lib/store.js
+++ packages/grid/frontend/src/lib/store.js
@@ -5,23 +5,23 @@ export const credentials = writable('');
 export const jsSerde = writable('');
 
 export const store = writable({
-	jsserde: ''
+  jsserde: ''
 });
 
 export async function getSerde() {
-	let newStore = '';
-	store.subscribe((value) => {
-		newStore = value;
-	});
+  let newStore = '';
+  store.subscribe((value) => {
+    newStore = value;
+  });
 
-	if (!newStore.jsserde) {
-		let type_bank = await fetch('http://localhost:8081/api/v1/syft/serde')
-			.then((response) => response.json())
-			.then(function (response) {
-				return response['bank'];
-			});
-		newStore.jsserde = new JSSerde(type_bank);
-		store.set(newStore);
-	}
-	return newStore.jsserde;
+  if (!newStore.jsserde) {
+    let type_bank = await fetch('http://localhost:8081/api/v1/syft/serde')
+      .then((response) => response.json())
+      .then(function (response) {
+        return response['bank'];
+      });
+    newStore.jsserde = new JSSerde(type_bank);
+    store.set(newStore);
+  }
+  return newStore.jsserde;
 }
